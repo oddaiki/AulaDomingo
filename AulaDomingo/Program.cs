87cd60b4710673a2be565416a5b55050a46835cc
Vinícius <viniciusj.ferreira12@gmail.com>
2023-03-12 13:32:47 -0300

    Adicionar arquivos de projeto.

---
+++ AulaDomingo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaDomingo
+{
+    public class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            double idade;
+            double meses = 12;
+            double maioridade = 18;
+            string nome;
+
+            Console.WriteLine("Qual o seu nome? ");
+            nome = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Quantos anos você tem? ");
+            idade = Convert.ToDouble(Console.ReadLine());
+     
+        if (idade >= maioridade)
+        {
+                Console.WriteLine("Boa! " + nome + " ! já é de maior");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine(nome + " Falta(m) " + (maioridade - idade) + " anos e " +
+                    meses + " meses para completar a maioridade");
+                Console.ReadLine();
+            }
+        }
+    }
+}
